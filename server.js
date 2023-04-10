ba89c191be58f6c3ccc3fafebd108416c1602974
Lucas Werner <lwerner27@gmail.com>
2023-04-10 17:16:46 -0500

    Moved routes to the router.

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./database');
-const cardsController = require("./Controllers/CardsController");
-const { object } = require('webidl-conversions');
-
+const routes = require("./Router")
 
 // Load environment variables
 dotenv.config();
@@ -20,21 +18,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Setup the API router
+app.use('/api', routes)
+
 // Sample route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
-// Test controller functions
-app.get('/test', (req, res) => {
-    cardsController.getAllCardNames()
-    res.sendStatus(200)
-})
-
-// Testing req.params 
-app.get("/test/:cardId", (req, res) => {
-    cardsController.getCard(req, res)
-})
 
 // Start the server
 const port = process.env.PORT || 3000;
