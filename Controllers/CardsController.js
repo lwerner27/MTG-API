72a899298778a39465e98d64d7d0968eb719b298
Lucas Werner <lwerner27@gmail.com>
2023-04-10 17:18:52 -0500

    Started working on the getCardsByName function.

--- Controllers/CardsController.js
+++ Controllers/CardsController.js
@@ -1,9 +1,10 @@
 const { ObjectId } = require("bson");
 const { getDb } = require("../database")
+const fs = require("fs")
 
 
 module.exports = {
-    getAllCardNames: async () => {
+    getAllCardNames: async (req, res) => {
         Cards = getDb().collection('Cards')
         try {
             let count = 0
@@ -12,13 +13,15 @@ module.exports = {
                 count++
                 console.log(`${count}: ${element.name}, ${element._id}`)
             });
+            res.sendStatus(200)
             
         } catch (error) {
             console.error('Error getting documents:', error);
+            res.sendStatus(500)
         }
     }, 
 
-    getAllUniqueCardNames: async () => {
+    getAllUniqueCardNames: async (req, res) => {
         Cards = getDb().collection('Cards')
         try {
             let count = 0
@@ -28,9 +31,12 @@ module.exports = {
                 count++ 
                 console.log(`${count}: ${card}`)
             })
+
+            res.sendStatus(200)
             
         } catch (error) {
             console.error('Error getting documents:', error);
+            res.sendStatus(500)
         }
     },
 
@@ -38,5 +44,11 @@ module.exports = {
         Cards = getDb().collection('Cards')
         const card = await Cards.findOne({_id: new ObjectId(req.params.cardId)})
         res.redirect(`https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=${card.multiverseId}`)
+    },
+
+    getCardByName: async (req, res) => {
+        Cards = getDb().collection("Cards")
+        searchCards = await Cards.find({name:req.params.cardName})
+        res.sendStatus(200).send(searchedCards)
     }
 }
